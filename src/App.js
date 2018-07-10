391c8a426626c6147b7f7784b5fa6cd4a6110712
Colin Hayes <colin.hayes985@gmail.com>
2018-07-10 11:34:25 -0400

    Add header style/content

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        API-Party
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API-Party</h1>
+        </header>
       </div>
     );
   }
